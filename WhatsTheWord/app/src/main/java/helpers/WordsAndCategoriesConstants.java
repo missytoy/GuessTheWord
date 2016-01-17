768979c1fa6a1e7eee150d669e99b6d0f159643b
missytoy <missytoy@gmail.com>
2016-01-17 20:32:51 +0200

    Constants extracted in resource file.

--- WhatsTheWord/app/src/main/java/helpers/WordsAndCategoriesConstants.java
+++ WhatsTheWord/app/src/main/java/helpers/WordsAndCategoriesConstants.java
@@ -19,15 +19,15 @@ public class WordsAndCategoriesConstants {
 
     // Words
     public final static String[] OBJECTS_WORDS = new String[]{
-            "Microphone", "Piano", "Umbrella", "Bottle", "Neck", "Pillow", "Saxophone", "Condom",
-            "Table", "Chair", "Glass", "Straw", "Tree", "Wood", "Water", "Gas", "Fork", "Spoon", "PLate", "Stairs", "Phone", "Word", "Game", "Eye", "Nose",
+            "Microphone", "Piano", "Umbrella", "Neck", "Pillow", "Saxophone", "Condom",
+            "Table", "Chair", "Glass", "Straw", "Wood", "Water", "Gas", "Fork", "Spoon", "PLate", "Stairs", "Phone", "Word", "Game", "Eye", "Nose",
             "Glasses", "Nail", "Liver", "Forum", "Mummy", "Window", "Ninja", "T-shirt", "Skirt", "Dress", "Jeans", "Lamp", "Bulb", "Button",
             "Curtain", "Carpet", "Snow", "Rain", "Car", "Bus", "Elevator", "Gloves", "Scarf", "Computer", "Laptop", "Roof", "Sofa", "Coffee",
             "Traffic light", "Abs", "Boy", "Girl", "Escalator", "Brick", "Fire", "Flag", "Bottle", "Food", "Door",
             "Price", "Drink", "Song", "Scene", "Theatre", "Cinema", "Movie", "Actor", "Script", "Projector", "Bed", "TV", "Clothes", "Drawer", "Wardrobe",
-            "Washing machine", "Stove", "Heater", "Desk", "Teddy bear", "Clock", "Cable", "Web", "Mirror", "Toilet", "Hanger", "Blackboard",
+            "Washing machine", "Stove", "Heater", "Desk", "Teddy bear", "Clock", "Cable", "Mirror", "Toilet", "Hanger", "Blackboard",
             "Pencil", "Notebook", "Ear", "Rubber", "Eraser", "Family", "Folio", "Forehead", "Makeup",
-            "Cloud", "Tooth", "Garden", "Pool", "Flower", "Knife", "Box", "Bike", "Bracelet", "Ring", "Necklace", "Hat", "Jacket", "Shoe",
+            "Tooth", "Garden", "Pool", "Flower", "Knife", "Box", "Bike", "Bracelet", "Ring", "Necklace", "Hat", "Jacket", "Shoe",
             "Number", "Blanket", "Perfume", "Shampoo", "Soap", "Sink", "Poster", "Picture", "Chat", "Document", "Flashlight", "Money", "Address", "House", "Key", "Locker",};
     public final static String[] SONGS_WORDS = new String[]{"Eminem - My name is", "Dr DRE - Still DRE",
             "Rolling Stones - Satisfaction", "Red Hot Chili Peppers - Californication",
@@ -46,13 +46,13 @@ public class WordsAndCategoriesConstants {
             "Robin Schulz - Waves", "Rob Thomas - Lonely No More"
     };
 
-    public final static String[] ANIMALS_WORDS = new String[]{"Frog", "Cat", "Dog", "Snake", "Turtle",
+    public final static String[] ANIMALS_WORDS = new String[]{"Frog", "Cat", "Dog", "Turtle",
             "Tasmanian Devil", "Sloth", "Chicken",
             "Penguin", "Fox", "Wolf", "Elephant", "Bat", "Bear", "Beetle", "Bird", "Shark", "Bee", "Butterfly", "Camel", "Catfish", "Chameleon", "Cheetah", "Chinchilla", "Fish", "Cockroach", "Cow", "Coyote",
             "Crocodile", "Deer", "Dolphin", "Duck", "Dragonfly", "Eagle", "Giraffe", "Goose", "Pig", "Hamster", "Hippopotamus", "Horse", "Monkey", "Human", "Kangaroo",
             "Jellyfish", "Koala", "Tortoise", "Lion", "Lizard", "Llama", "Mouse", "Gorilla", "Mule", "Octopus", "Opossum", "Panther",
             "Pelican", "Puma", "Rabbit", "Rat", "Panda", "Scorpion", "Sheep", "Tiger", "Snail", "Snake", "Turkey",
-            "Parrot", "Meerkat", "Zebra", "Pigeon", "Peacock", "Bunny", "Puppy", "Kitten", "Donkey", "Goat", "Ram", "Bull", "Hedgehog", "Guinea pig",
+            "Parrot", "Meerkat", "Zebra", "Pigeon", "Peacock", "Bunny", "Puppy", "Kitten", "Donkey", "Goat", "Bull", "Hedgehog", "Guinea pig",
             "Ant", "Wasp", "Hyena", "Lioness", "Orangutan", "Schimpanse", "Spider", "Fly", "Firefly"};
     public final static String[] BG_STARS_WORDS = new String[]{"Слави Трифонов", "Стоичков", "Бербатов", "Гонзо", "Криско",
             "Лили Иванова", "Васил Найденов", "Емил Костадинов", "Красимир Балъков"};
@@ -64,11 +64,11 @@ public class WordsAndCategoriesConstants {
             "Господин за един ден", "Despicable me", "The Godfather", "The Shawshenk Redemption", "Borat", "Ну Погоди"};
     public final static String[] BGSONGS_WORDS = new String[]{"Йовано Йованке", "Нон стоп", "Писна ми", "Ах, морето", "Луди жаби",
             "Назад назад, моме Калино"};
-    public final static String[] STARS_WORDS = new String[]{"Messi", "Ronaldo", "Steve Jobs", "Robert De Niro", "Bastian Schweinsteiger",
+    public final static String[] STARS_WORDS = new String[]{"Messi", "Ronaldo", "Robert De Niro", "Bastian Schweinsteiger",
             "Madonna", "Harrison Ford"};
 
     public final static String[] IT_WORDS = new String[]{"Java", "Android", "C#", "Windows", "Console", "Web", "HTML", "CSS",
-            "PHP", "Python", "Ruby", "Bug", "C++", "Objective C", "JavaScript", "Program", "Mouse", "StartUp", "Computer", "IOS", "Linux",
+            "PHP", "Python", "Ruby", "Bug", "C++", "Objective C", "JavaScript", "Program", "StartUp", "IOS", "Linux",
             "Mac", "Compilation", "Database", "Unit test", "Integration test", "MySql", "SQLite", "Quality Code", "MS SQL", "XAML", "XML",
             "Post", "Get", "Put", "404", "201", "501", "301", "Install", "Uninstall", "CMD", "Router", "Firewall", "Firefox", "Edge",
             "Opera", "Safari", "Explorer", "Chrome", "Google", "Video card", "Ram", "CPU", "Motherboard", "Keyboard", "USB",
@@ -80,8 +80,8 @@ public class WordsAndCategoriesConstants {
             "Dependency inversion", "Boy scout rule", "Random", "Tree", "Graph", "Algorithm", "Dijkstra", "Source control system", "Binary search",
             "Bit", "Byte", "Ninject", "Automapper", "Reflection", "Node", "MVC", "MVVM", "Web forms", ".NET", "Artificial intelligence",
             "Code", "Validation", "UI", "Data binding", "Web site", "TCP", "IP", "Wi-Fi", "Encryption", "Photoshop", "Graphic Design",
-            "Sublime text", "Webstorm", "Jetbrains", "Visual Studio", "Atom","Digital Signature","Data mining","Bitcoin",".cs","DDL",
-            "DML", "CRUD", ".dll", "Thread","goto"};
+            "Sublime text", "Webstorm", "Jetbrains", "Visual Studio", "Atom", "Digital Signature", "Data mining", "Bitcoin", ".cs", "DDL",
+            "DML", "CRUD", ".dll", "Thread", "goto"};
 
 
     public final static HashMap<String, String[]> WORDS_BY_CATEGORYNAME = new HashMap<String, String[]>() {{
